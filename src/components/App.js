3aa5b5436b5bb36ca9b52ad4fdf5faf3bfc2408c
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-06-16 13:58:43 +0300

    adding files

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import { Layout } from "components/Layout/Layout";
-import { useEffect } from "react";
-import { fetchContacts } from "redux/operations";
-import { useDispatch, useSelector } from "react-redux";
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
+import Layout from './Layout/Layout';
 import Filter from './Filter';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "redux/operations";
 import { getError, getIsLoading } from "redux/selectors";
 
-export function App() {
+export const App = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
   const error = useSelector(getError);
@@ -15,14 +15,15 @@ export function App() {
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
   return (
-      <Layout>
+    <Layout>
         <h1>Phonebook</h1>
         <ContactForm />
-        {isLoading && !error && <b>Making changes to the phone book...</b>}
+        {isLoading && !error && <b>Adding contact to phonebook...</b>}
         <h2>Contacts</h2>
         <Filter />
         <ContactList />
       </Layout>
   );
-}
+};
